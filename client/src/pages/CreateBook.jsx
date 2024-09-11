afac7ae42f8310a4b568cbdd175061b63633ff4d
AKHILESH YADAV <122615318+akhilesh3841@users.noreply.github.com>
2024-09-11 22:58:18 +0530

    Update CreateBook.jsx

--- client/src/pages/CreateBook.jsx
+++ client/src/pages/CreateBook.jsx
@@ -20,7 +20,7 @@ const CreateBook = () => {
     e.preventDefault();
     setLoading(true);
     try {
-      const res = await axios.post('https://bookstore-server-po8m.onrender.com/api/v1/create', {
+      const res = await axios.post('https://mernnew-backend.onrender.com/api/v1/create', {
         title,
         genre,
         author,
